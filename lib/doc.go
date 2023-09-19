3648b191628490ac0c4d91ff914ef83cda0dfa88
liubingbing2 <liubingbing2@tal.com>
2023-09-19 12:06:32 +0800

    feat: libx264支持macos windows

--- lib/doc.go
+++ lib/doc.go
@@ -1,4 +1,5 @@
 package lib
 
+//go:generate go install github.com/go-bindata/go-bindata@latest
 //go:generate go-bindata -tags=windows -nomemcopy -o=libx264_windows.go -pkg=lib libx264-164.dll
 //go:generate go-bindata -tags=darwin -nomemcopy -o=libx264_darwin.go -pkg=lib libx264.164.dylib

07b00a375c290443300bfb5355e2d9ba921af4c7
lbbniu <lbbniu@gmail.com>
2023-09-21 21:32:09 +0800

    取消优化处理

--- lib/lib.go
+++ lib/lib.go
@@ -3,26 +3,27 @@ package lib
 import (
 	"os"
 	"path"
+	"sync"
 
 	"github.com/mitchellh/go-homedir"
 	"github.com/moonfdd/x264-go/libx264common"
 )
 
+var once sync.Once
+
 func Init() {
-	dir, err := homedir.Dir()
-	if err != nil {
-		panic(err)
-	}
-	libPath := path.Join(dir, ".lbbniu")
-	libFile := path.Join(libPath, libName)
-	if _, err = os.Stat(libFile); err == nil {
-		return
-	}
-	if err = RestoreAssets(libPath, libName); err != nil {
-		panic(err)
-	}
-	if err = os.Setenv("Path", os.Getenv("Path")+";./lib"); err != nil {
-		panic(err)
-	}
-	libx264common.SetLibx264Path(libFile)
+	once.Do(func() {
+		dir, err := homedir.Dir()
+		if err != nil {
+			panic(err)
+		}
+		libPath := path.Join(dir, ".lbbniu")
+		if err = RestoreAssets(libPath, libName); err != nil {
+			panic(err)
+		}
+		if err = os.Setenv("Path", os.Getenv("Path")+";./lib"); err != nil {
+			panic(err)
+		}
+		libx264common.SetLibx264Path(path.Join(libPath, libName))
+	})
 }
